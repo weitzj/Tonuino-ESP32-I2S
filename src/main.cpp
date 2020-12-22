ed62788dea816401aa9244b4a56da28229c5b3d4
Jan Weitz <me@janweitz.de>
2020-12-22 13:59:44 +0100

    led brightness

--- src/main.cpp
+++ src/main.cpp
@@ -156,7 +156,7 @@ typedef struct {
 // Configuration of initial values (for the first start) goes here....
 // There's no need to change them here as they can be configured via webinterface
 // Neopixel
-uint8_t initialLedBrightness = 16;                      // Initial brightness of Neopixel
+uint8_t initialLedBrightness = 150;                      // Initial brightness of Neopixel
 uint8_t ledBrightness = initialLedBrightness;
 uint8_t nightLedBrightness = 2;                         // Brightness of Neopixel in nightmode
 
